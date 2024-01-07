14711bb820fec021024cf8dbefb1f7ee9afdac63
xalnite <xalnite@gmail.com>
2024-01-07 20:13:07 +0900

    test: test the initial state

--- lib/src/test/java/com/github/xalnite/util/OptionTest.java
+++ lib/src/test/java/com/github/xalnite/util/OptionTest.java
@@ -3,12 +3,15 @@
  */
 package com.github.xalnite.util;
 
-import org.testng.annotations.*;
 import static org.testng.Assert.*;
+import org.testng.annotations.Test;
 
 public class OptionTest {
-    @Test public void someLibraryMethodReturnsTrue() {
-        Option classUnderTest = new Option();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+    @Test
+    public void initialState() {
+        assertEquals(Option.optarg, null);
+        assertTrue(Option.opterr);
+        assertEquals(Option.optind, 0);
+        assertEquals(Option.optopt, Option.EOF);
     }
 }
